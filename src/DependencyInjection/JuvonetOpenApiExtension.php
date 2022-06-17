813a8cdf96284d14333987484b66efbc92f4e9aa
Samu Juvonen <samu.juvonen@gmail.com>
2022-06-17 19:01:50 +0700

    Remove tight coupling between the documentation generator and router loader

--- src/DependencyInjection/JuvonetOpenApiExtension.php
+++ src/DependencyInjection/JuvonetOpenApiExtension.php
@@ -2,9 +2,12 @@
 
 namespace Juvonet\OpenApiBundle\DependencyInjection;
 
+use Juvonet\OpenApi\Loader\ClassDocumentationLoader;
+use Juvonet\OpenApiBundle\Routing\OpenApiRouteLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class JuvonetOpenApiExtension extends Extension
@@ -25,5 +28,47 @@ class JuvonetOpenApiExtension extends Extension
         $container->setParameter('juvonet.openapi.public_schema_namespaces', $processed['discovery']['public_schema']['namespaces']);
         $container->setParameter('juvonet.openapi.public_schema_namespaces', $processed['discovery']['public_schema']['namespaces']);
         $container->setParameter('juvonet.openapi.tags.trim_prefixes', $processed['tags']['trim_prefixes']);
+
+        $this->processRouteLoader($container, $processed);
+    }
+
+    private function processRouteLoader(ContainerBuilder $container, array $processed): void
+    {
+        /**
+         * Each route loader needs a dedicated instance of ClassDocumentationLoader
+         * as well, since filtering paths is done in there.
+         */
+
+        if (empty($processed['routing']['profiles'])) {
+            $processed['routing']['profiles']['default'] = [
+                'paths' => $processed['discovery']['class_loader']['paths'],
+                'exclude' => [],
+            ];
+        }
+
+        foreach ($processed['routing']['profiles'] as $profile => $config) {
+            $routeLoaderId = "juvonet.openapi.routing.route_loader.{$profile}";
+            $documentationLoaderId = "juvonet.openapi.routing.class_documentation_loader.{$profile}";
+
+            $container->register($documentationLoaderId, ClassDocumentationLoader::class)
+                ->setAutowired(true)
+                ->setAutoconfigured(false)
+                ->setArgument('$paths', $config['paths'])
+                ->setArgument('$exclude', $config['exclude'])
+                ;
+
+            if (!$container->has(OpenApiRouteLoader::class)) {
+                $routeLoaderDefinition = $container->register(OpenApiRouteLoader::class);
+                $container->setAlias($routeLoaderId, OpenApiRouteLoader::class);
+            } else {
+                $routeLoaderDefinition = $container->register($routeLoaderId, OpenApiRouteLoader::class);
+            }
+
+            $routeLoaderDefinition
+                ->setAutowired(true)
+                ->setAutoconfigured(true)
+                ->setArgument('$loader', new Reference($documentationLoaderId))
+                ;
+        }
     }
 }
